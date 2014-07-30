b27e6f9e347f6c8dadc7958508faae1c8ae9abc2
Arnaud Pichon <arnaud@onesime.fr>
2014-07-30 02:21:57 +0200

    longer random

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,14 +4,10 @@ var chuck = require('./chuck');
 var say = require('./say');
 var hashStringToColor = require('./string-2-color');
 
-chuck(function(data) {
-  say(data.value.joke);
-});
-
 var randomizeRequest = function(){
   chuck(function(data) {
     var joke = data.value.joke.replace('&quot;', '');
-    var time =  Math.random() * 15000 + 3000 + joke.length + 100 ;
+    var time =  Math.random() * 150000 + 3000 + joke.length + 100 ;
     console.log(joke);
     say(joke);
     document.body.style.backgroundColor = hashStringToColor(joke);
--- app/scripts/string-2-color.js
+++ app/scripts/string-2-color.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function djb2(str){
   var hash = 5381;
   for (var i = 0; i < str.length; i++) {
@@ -7,9 +9,9 @@ function djb2(str){
 }
 
 module.exports = function(str) {
-    var hash = djb2(str);
-    var r = (hash & 0xFF0000) >> 16;
-    var g = (hash & 0x00FF00) >> 8;
-    var b = hash & 0x0000FF;
-    return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
-  }
+  var hash = djb2(str);
+  var r = (hash & 0xFF0000) >> 16;
+  var g = (hash & 0x00FF00) >> 8;
+  var b = hash & 0x0000FF;
+  return '#' + ('0' + r.toString(16)).substr(-2) + ('0' + g.toString(16)).substr(-2) + ('0' + b.toString(16)).substr(-2);
+};
