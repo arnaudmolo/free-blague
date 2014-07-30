0a84bd880fede8a37a8abc857b9dfe37c215d71e
Arnaud Pichon <arnaud@onesime.fr>
2014-07-30 02:08:37 +0200

    color

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,10 @@ chuck(function(data) {
 
 var randomizeRequest = function(){
   chuck(function(data) {
-    var time =  Math.random() * 15000 + 3000 + data.value.joke.length + 100 ;
-    console.log(time);
-    say(data.value.joke);
+    var joke = data.value.joke;
+    var time =  Math.random() * 15000 + 3000 + joke.length + 100 ;
+    say();
+    document.body.style.backgroundColor = hashStringToColor(joke);
     setTimeout(function(){
       randomizeRequest();
     }, time);
@@ -19,3 +20,21 @@ var randomizeRequest = function(){
 };
 
 randomizeRequest();
+
+function djb2(str){
+  var hash = 5381;
+  for (var i = 0; i < str.length; i++) {
+    hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
+  }
+  return hash;
+}
+
+function hashStringToColor(str) {
+  var hash = djb2(str);
+  var r = (hash & 0xFF0000) >> 16;
+  var g = (hash & 0x00FF00) >> 8;
+  var b = hash & 0x0000FF;
+  return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
+}
+
+console.log(hashStringToColor('kouk'));
--- app/scripts/say.js
+++ app/scripts/say.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function(string){
-  console.log(string);
   var msg = new window.SpeechSynthesisUtterance(string);
   window.speechSynthesis.speak(msg);
 };
