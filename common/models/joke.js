947741f1873404a57f9c74cb562e16e98e136903
Arnaud Pichon <arnaud@onesime.fr>
2014-10-08 16:18:24 +0200

    add a mute button

--- common/models/joke.js
+++ common/models/joke.js
@@ -6,32 +6,13 @@ module.exports = function(Joke){
   Joke.definition.properties.random.default = Math.random;
 
   Joke.random = function(cb) {
-    var rand;
-    rand = Math.random();
 
-    Joke.findOne({
-      where: {
-        random: {
-          lte: rand
-        }
-      }
-    }, function(err, res) {
-
-      console.log(rand);
-
-      if (res === null) {
-        Joke.findOne({
-          where: {
-            random: {
-              gte: rand
-            }
-          }
-        }, function(err, res) {
-          cb(null, res);
-        });
-        return;
-      }
-      cb(null, res);
+    Joke.count(function(err, res){
+      var rand;
+      rand = Math.floor(Math.round(Math.random() * res));
+      Joke.findOne({skip: rand}, function(err, res){
+        cb(null, res);
+      });
     });
 
   };
