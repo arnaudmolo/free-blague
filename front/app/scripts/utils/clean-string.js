9c85f17dce374884a2628a5b16093fc65ed1c508
Clément Procureur <mc.flurry@hotmail.fr>
2014-10-25 11:49:14 +0200

    pull rewuest review

---
+++ front/app/scripts/utils/clean-string.js
@@ -0,0 +1,45 @@
+
+/**
+* Clean sentence of his typos.
+*
+* @param {String} A sentence.
+* @return {String} A clean sentence.
+*/
+
+function cleanSentence(sentence){
+
+	sentence = sentence.replace(/\?+/g, '?');                        // remove multiple ?
+	sentence = sentence.replace(/\!+/g, '!');                        // remove multiple !
+	sentence = sentence.replace(/\,+\,(?!\d)/g, ', ');               // remove multiple , and be careful of floats
+	sentence = sentence.replace(/\s\,+/g, ', ');                     // remove space before comma
+	sentence = sentence.replace(/\s+/g, ' ');                        // remove multiple spaces
+	sentence = sentence.replace(/\s\.{2,}/g, '...');                 // force 3 dots
+	sentence = sentence.replace(/\s\.+(?!\.{2})/g, '.');             // remove space before dot
+	sentence = sentence.replace(/(^\s+)|(\s+$)/g, '');               // remove multiple begining or ending spaces
+	sentence = sentence.toLowerCase();                               // remove capital letters
+	sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1); // Capitalize first letter
+
+	return sentence;
+}
+
+
+/**
+* Clean joke of his typos.
+*
+* @param {String} A multi sentence or not String.
+* @return {String} A clean multi sentence string.
+*/
+
+function cleanJoke(joke_content){
+
+	joke_content = joke_content.match( /[^\.!\?]+[\.!\?]+/g );
+	for(i=0; i<joke_content.length;i++){
+	  joke_content[i] = cleanSentence(joke_content[i])
+	}
+
+	joke_content = joke_content.join(' ');
+
+	return joke_content;
+}
+
+module.exports = cleanJoke;
