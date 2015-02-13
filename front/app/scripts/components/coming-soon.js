efb25906443d5b12065a0aca74099e7425a85325
heymath <dutto.mathieu@gmail.com>
2015-02-13 11:56:08 +0100

    Add textarea joke for coming soon + better responsive

--- front/app/scripts/components/coming-soon.js
+++ front/app/scripts/components/coming-soon.js
@@ -28,6 +28,7 @@ export default class ComingSoon extends React.Component {
     super(props);
     this.state = getDefaultStates();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleSubmit(event) {
@@ -52,9 +53,11 @@ export default class ComingSoon extends React.Component {
 
   render() {
 
-    let paragraph;
+    let paragraph, jokeLengthLimit, baseInputClass;
 
     paragraph = "Pour être informé de l'ouverture de la plateforme et obtenir une invitation à la version bêta :";
+    baseInputClass = "input input--rounded input--big input--full-w";
+    jokeLengthLimit = 350;
 
     return (
       <div className={
@@ -66,22 +69,32 @@ export default class ComingSoon extends React.Component {
         <header className="header--main header--absolute">
           <i className="header__logo icon-already-cool"></i>
         </header>
-        <section className="coming-soon__content table__cell table--align-middle">
+        <section className="coming-soon__content">
           <h2>Tribute to humour</h2>
           <h3>Coming soon.</h3>
           <p><strong>{paragraph}</strong></p>
           <form className="coming-soon__form form isVertical" onSubmit={this.handleSubmit}>
             <div className="form__block">
-              <input
-                className="input input--rounded input--big input--full-w"
-                type="text"
-                placeholder="Envoyez votre meilleur blague !"
-                required
-                onChange={this.handleInputChange('joke')}
-                value={this.state.joke} />
+              <div className="input-limited">
+                <textarea
+                  className={baseInputClass + " input--resize-v input--optional"}
+                  type="text"
+                  placeholder="Envoyez votre meilleur blague !"
+                  required="required"
+                  maxLength={jokeLengthLimit}
+                  onChange={this.handleInputChange('joke')}
+                  value={this.state.joke} />
+                <span className="input-limited__limit">{jokeLengthLimit - this.state.joke.length}</span>
+              </div>
             </div>
             <div className="form__block">
-              <input className="input input--rounded input--big input--full-w" type="email" placeholder="Email" required onChange={this.handleInputChange('email')} value={this.state.email}/>
+              <input
+                className={baseInputClass}
+                type="email"
+                placeholder="Email"
+                required="required"
+                onChange={this.handleInputChange('email')}
+                value={this.state.email} />
             </div>
             <div className="form__block">
               <input type="submit" className="button button--big button--important button--full-w" value="Obtenir mon invitation" />
